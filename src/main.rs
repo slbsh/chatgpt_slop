7c130216c7e693cdec930145ec7ee65dfa79ec39
slbsh <crafter.antek@gmail.com>
2024-12-18 17:52:26 +0100

    change: switch to ms azure for tts

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,12 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Default)]
 struct Config {
-	#[serde(deserialize_with = "api_key")]
-	api_key:    Box<str>,
+	#[serde(deserialize_with = "openai_key")]
+	openai_key:    Box<str>,
+	azure_key:     Box<str>,
+	azure_region:  Box<str>,
+	#[serde(default)]
+	azure_voice:   Box<str>,
 	#[serde(default)]
 	#[serde(deserialize_with = "prompt")]
 	prompt:     Box<str>,
@@ -22,7 +26,7 @@ struct Config {
 	keycode:    Option<NonZero<u32>>,
 }
 
-fn api_key<'de, D: serde::Deserializer<'de>>(de: D) -> Result<Box<str>, D::Error> {
+fn openai_key<'de, D: serde::Deserializer<'de>>(de: D) -> Result<Box<str>, D::Error> {
 	let s: String = Deserialize::deserialize(de)?;
 	Ok(Box::from(format!("Bearer {s}")))
 }
@@ -54,11 +58,6 @@ static CONFIG: LazyLock<Config> = LazyLock::new(||
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	if std::env::args().nth(1) == Some(String::from("keytest")) {
-		rdev::listen(|e| println!("{e:?}")).unwrap();
-		return Ok(());
-	}
-
 	let mut messages: VecDeque<String> = VecDeque::new();
 	let client = reqwest::Client::new();
 
@@ -79,7 +78,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	loop {
 		println!("Press key to start");
 		rx.recv().unwrap();
-		
+
 		let mut cmd = std::process::Command::new("ffmpeg")
 			.args(["-y", "-loglevel", "error", "-f", &CONFIG.backend, "-i", &CONFIG.device, &CONFIG.audio_file])
 			.stdin(std::process::Stdio::piped())
@@ -93,7 +92,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 		let serde_json::Value::String(resp) = 
 			check_err(client.post("https://api.openai.com/v1/audio/transcriptions")
-				.header("Authorization", &*CONFIG.api_key)
+				.header("Authorization", &*CONFIG.openai_key)
 				.multipart(reqwest::multipart::Form::new()
 					.file("file", &*CONFIG.audio_file).await?
 					.text("model", "whisper-1"))
@@ -107,7 +106,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 		let serde_json::Value::String(resp) = 
 			check_err(client.post("https://api.openai.com/v1/chat/completions")
-				.header("Authorization", &*CONFIG.api_key)
+				.header("Authorization", &*CONFIG.openai_key)
 				.header("Content-Type", "application/json")
 				.body(format!(r#"{{ "model": "gpt-4o", "messages": [{}{} {}] }}"#,
 					CONFIG.prompt, if !CONFIG.prompt.is_empty() { "," } else { "" },
@@ -124,10 +123,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 		println!("Response: {resp}");
 		if messages.len() >= CONFIG.msg_limit { messages.pop_front(); }
 
-		let resp = check_err(client.post("https://api.openai.com/v1/audio/speech")
-			.header("Authorization", &*CONFIG.api_key)
-			.header("Content-Type", "application/json")
-			.body(serde_json::json!({ "model": "tts-1", "input": resp, "voice": "onyx" }).to_string())
+		let resp = check_err(client.post(format!("https://{}.tts.speech.microsoft.com/cognitiveservices/v1", &*CONFIG.azure_region))
+			.header("Ocp-Apim-Subscription-Key", &*CONFIG.azure_key)
+			.header("Content-Type", "application/ssml+xml")
+			.header("X-Microsoft-OutputFormat", "audio-48khz-96kbitrate-mono-mp3")
+			.header("User-Agent", "curl")
+			.body(format!(" <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xmlns:mstts='http://www.w3.org/2001/mstts' xml:lang='en-US'><voice name='{}'>{}</voice></speak>",
+				if CONFIG.azure_voice.is_empty() { "en-US-JasonNeural" } else { &*CONFIG.azure_voice },
+				match resp.starts_with(":").then(|| resp.find(" ").unwrap_or(resp.len()) + 1) {
+					None      => resp,
+					Some(pos) => format!("<mstts:express-as style='{}'>{}</mstts:express-as>", &resp[1..pos], &resp[pos+1..]),
+				}))
 			.send().await?).await
 			.bytes().await?;
 
